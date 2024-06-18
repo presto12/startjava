8c80e4e445b373bc90c99a5727089ec84b11b742
presto12 <40945790+presto12@users.noreply.github.com>
2024-06-18 21:45:26 +0300

    Изменил вывод текста, отображаемого в консоли

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -1,16 +1,6 @@
-import java.util.Scanner;
-
 public class MyFirstApp {   
     public static void main(String[] args) {
-        System.out.println("Написано однажды, работает везде");
-        Scanner console = new Scanner(System.in, "cp866");
-        System.out.print("Введите имя:");
-        System.out.println(console.nextLine());
-
-        int sum = 5;
-        int a = 1;
-        int b = 10;
-        sum = a + b;
-        System.out.println(sum);
+        System.out.print("Написано однажды, ");
+        System.out.println("работает везде");
     }
 }
